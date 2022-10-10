b5f921fd272a1af0d5aaa9a65bae0faf940f64f2
Henry Le <henryle10@gmail.com>
2022-10-10 12:23:27 -0700

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAuthentication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AspNetUsers>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Data/AspNetUsers.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorAuthentication.Data
+{
+    public class AspNetUsers : IdentityUser
+    {
+        public string School { get; set; }
+    }
+}
